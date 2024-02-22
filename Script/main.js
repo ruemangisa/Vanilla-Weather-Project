5c63acf71b96eb40275f74be1aba179d405a4a5d
ruemangisa <77462324+ruemangisa@users.noreply.github.com>
2024-02-22 15:13:07 +0200

    Updated js to accommodate searches

--- Script/main.js
+++ Script/main.js
@@ -1,8 +1,21 @@
+function refreshWeather(response) {
+    let tempElement = document.querySelector(".weather-temp");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#searchOutput");
+    
+    cityElement.innerHTML = response.data.city;
+    tempElement.innerHTML = Math.round(temperature);
+}
+function searchCity(searchOutput) {
+    let apiKey = "c1ffea9484c65b5596835a015t56o314";
+    let apiURL = `https://api.shecodes.io/weather/v1/current?query=${searchOutput}&key=${apiKey}`;
+    axios.get(apiURL).then(refreshWeather);
+    
+}
 function handleSeachSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-input");
-    let cityElement = document.querySelector("#search-output");
-    cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value)
 }
 
 
